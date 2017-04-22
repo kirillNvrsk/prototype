b4674e8857e58e970bf920c223e3f882f1149131
kirillNvrsk <19docone99@gmail.com>
2017-04-22 16:40:05 +0300

    22.04
    book_update

--- app/src/main/java/com/kirillprog/s1/prot/Book.java
+++ app/src/main/java/com/kirillprog/s1/prot/Book.java
@@ -14,11 +14,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.SimpleExpandableListAdapter;
 import android.widget.TabHost;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Book extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String[] mGroupsArray = new String[] { "Зима", "Весна", "Лето", "Осень" };
+
+    private String[] mWinterMonthsArray = new String[] { "Декабрь"};
+    private String[] mSpringMonthsArray = new String[] { "Март"};
+    private String[] mSummerMonthsArray = new String[] { "Июнь"};
+    private String[] mAutumnMonthsArray = new String[] { "Сентябрь"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +59,82 @@ public class Book extends AppCompatActivity
         tabSpec.setIndicator("Python 3");
         tabHost.addTab(tabSpec);
         tabHost.setCurrentTab(0);
+
+
+        Map<String, String> map;
+        // коллекция для групп
+        ArrayList<Map<String, String>> groupDataList = new ArrayList<>();
+        // заполняем коллекцию групп из массива с названиями групп
+
+        for (String group : mGroupsArray) {
+            // заполняем список атрибутов для каждой группы
+            map = new HashMap<>();
+            map.put("groupName", group); // время года
+            groupDataList.add(map);
+        }
+
+        // список атрибутов групп для чтения
+        String groupFrom[] = new String[] { "groupName" };
+        // список ID view-элементов, в которые будет помещены атрибуты групп
+        int groupTo[] = new int[] { android.R.id.text1 };
+
+        // создаем общую коллекцию для коллекций элементов
+        ArrayList<ArrayList<Map<String, String>>> сhildDataList = new ArrayList<>();
+
+        // в итоге получится сhildDataList = ArrayList<сhildDataItemList>
+
+        // создаем коллекцию элементов для первой группы
+        ArrayList<Map<String, String>> сhildDataItemList = new ArrayList<>();
+        // заполняем список атрибутов для каждого элемента
+        for (String month : mWinterMonthsArray) {
+            map = new HashMap<>();
+            map.put("monthName", month); // название месяца
+            сhildDataItemList.add(map);
+        }
+        // добавляем в коллекцию коллекций
+        сhildDataList.add(сhildDataItemList);
+
+        // создаем коллекцию элементов для второй группы
+        сhildDataItemList = new ArrayList<>();
+        for (String month : mSpringMonthsArray) {
+            map = new HashMap<>();
+            map.put("monthName", month);
+            сhildDataItemList.add(map);
+        }
+        сhildDataList.add(сhildDataItemList);
+
+        // создаем коллекцию элементов для третьей группы
+        сhildDataItemList = new ArrayList<>();
+        for (String month : mSummerMonthsArray) {
+            map = new HashMap<>();
+            map.put("monthName", month);
+            сhildDataItemList.add(map);
+        }
+        сhildDataList.add(сhildDataItemList);
+
+        // создаем коллекцию элементов для четвертой группы
+        сhildDataItemList = new ArrayList<>();
+        for (String month : mAutumnMonthsArray) {
+            map = new HashMap<>();
+            map.put("monthName", month);
+            сhildDataItemList.add(map);
+        }
+        сhildDataList.add(сhildDataItemList);
+
+        // список атрибутов элементов для чтения
+        String childFrom[] = new String[] { "monthName" };
+        // список ID view-элементов, в которые будет помещены атрибуты
+        // элементов
+        int childTo[] = new int[] { android.R.id.text1 };
+
+        SimpleExpandableListAdapter adapter1 = new SimpleExpandableListAdapter(
+                this, groupDataList,
+                android.R.layout.simple_expandable_list_item_1, groupFrom,
+                groupTo, сhildDataList, android.R.layout.simple_list_item_1,
+                childFrom, childTo);
+
+        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.cpp_list);
+        expandableListView.setAdapter(adapter1);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
